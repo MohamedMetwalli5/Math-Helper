bb4ae7f83d3c0afd7fb40b1cd756875b313cb0a0
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2020-12-12 09:38:37 +0200

    Add files via upload

---
+++ MathHelper/src/module-info.java
@@ -0,0 +1,3 @@
+module MathHelper {
+	requires java.desktop;
+}
---
+++ MathHelper/src/program/Main.java
@@ -0,0 +1,21 @@
+package program;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MainMenu m = new MainMenu();
+		
+		/*
+		double[][] a = {{25,5,1},{64,8,1},{144,12,1}};
+		double[] b = {106.8,177.2,279.2};
+		
+		Operation p = new Operation();
+		double[] x = p.GaussElimination(a, b);
+		for(int i=0;i<x.length;i++) {
+			System.out.print(x[i] + " ");
+		}
+		*/
+		
+	}
+
+}
