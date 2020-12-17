af9db00d1c559eb231dba0334fbf29b911604e50
Momen Ibrahim Fawzy <58600213+Momen-Ibrahim-Fawzy@users.noreply.github.com>
2020-12-17 03:34:26 +0200

    Update Result.java

--- MathHelper/src momen last eddition/program/Result.java
+++ MathHelper/src momen last eddition/program/Result.java
@@ -1,18 +1,20 @@
 package program;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Result extends JFrame{
     JFrame f = new JFrame();
     JPanel p = new JPanel();
-
-	public Result(double[] x, double[][] lu) { 
-	    this.setTitle("Math Helper");
+    JLabel l;
+    public Result(double[] x, double[][] lu) {
+        this.setTitle("Math Helper");
         this.setSize(800, 800);
         this.setVisible(true);
         this.setResizable(false);
@@ -20,14 +22,27 @@ public class Result extends JFrame{
         this.setLocation(400, 10);
         p.setSize(800, 800);
         p.setBackground(Color.orange);
-        p.setLayout(null);		
-	  
-	    
-	    this.add(p);
+        p.setLayout(null);
+
+        if(lu == null) {
+            int j = 0, i=50;
+            while(j < x.length && i<p.getHeight()) {
+                l = new JLabel("X"+(j+1) +" = "+ x[j]);
+                l.setBounds(100, i, 180, 60);
+                l.setForeground(Color.red);
+                l.setFont(new Font("atilic",Font.BOLD,40));
+                j++;
+                i += 80;
+                p.add(l);
+            }
+        }else {
+
+        }
+        this.add(p);
+    }
 
 
-  }
-	 	
+
 }
 
 
